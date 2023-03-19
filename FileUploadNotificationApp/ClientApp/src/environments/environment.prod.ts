e8a53ed92105fa6141c4ee397172b5ab1970793b
juniorC0 <gavrilyukmaks0@gmail.com>
2023-03-19 09:38:47 +0200

    Change Url for production

--- FileUploadNotificationApp/ClientApp/src/environments/environment.prod.ts
+++ FileUploadNotificationApp/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'fileuploadnotificationapp20230314225531.azurewebsites.net',
+  apiUrl: 'https://fileuploadnotificationapp.azurewebsites.net/',
 };
